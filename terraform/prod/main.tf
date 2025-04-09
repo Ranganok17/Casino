c0181cd23c182d605c107dda84835384043f5585
RaduTache <radu_tache@yahoo.com>
2025-04-10 00:23:58 +0300

    Adjusting lamda directory path

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -69,8 +69,8 @@ resource "aws_lambda_function" "dataProcessor" {
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  filename         = "${path.module}/../lambda_function.zip"  
-  source_code_hash = filebase64sha256("${path.module}/../lambda_function.zip")
+  filename         = "${path.module}/../../lambda//deployment_package.zip"  
+  source_code_hash = filebase64sha256("${path.module}/../../lambda/deployment_package.zip")
 
   tracing_config {
     mode = "Active"
